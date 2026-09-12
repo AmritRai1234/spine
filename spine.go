39af8cd7ef87d49bbf3e0db5954cf78a4aa822f1
AmritRai <amritrai@users.noreply.github.com>
2026-09-11 23:02:46 -0600

    docs: version bump to v3.0.9 (README header + spine.go)

--- spine.go
+++ spine.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version represents the current release version of Spine.
-const Version = "3.0.8"
+const Version = "3.0.9"
 
 // Type aliases for top-level engine & manifest components.
 type Engine = engine.Engine
